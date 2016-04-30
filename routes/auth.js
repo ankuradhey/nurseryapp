e9c857d5ee6d30f5c6a6a0f7f714bf7bdf3ca2e8
ankits <ankuradhey@gmail.com>
2016-04-30 10:30:04 +0530

    authorized properly - different mddleware created for authorization

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,8 @@
 var jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 var response = {error:true,success:false,message:'Oops! Some error occurred',code:400}
+var userModel = require("../models/user.js");
+validate = require('validate.js');
+
 var auth = {
     login: function(req, res){
         var username = req.body.user_email || '';
@@ -24,7 +27,69 @@ var auth = {
             this.generateToken(req.body.userParams);
         }
     },
-    generateToken: function(user){
-        
+    validate: function(username, password){
+        //username is email here
+        var invalid = validate.single(username, {presence:true, email:true});
+        if(!invalid){
+            userModel.loginCheck(username, password, function(err, rows){
+                if(err){
+                    res.status(500);
+                    response.message = 'Oops! Some error occurred';
+                    response.code = 500;
+                    response.errors = err;
+                    console.log(err);
+                    res.json(JSON.stringify(response));
+                }else{
+                    if(rows.length){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+            });
+        }else{
+            res.status(402);
+            response.code = 402;
+            response.message = 'Invalid Email address';
+            res.json(JSON.stringify(response));
+            return;
+        }
+    },
+    validateUser: function(username){
+        var invalid = validate.single(username, {presence:true, email:true});
+        if(!invalid){
+            userModel.validateUser(username, username, function(err, rows){
+                if(err){
+                    res.status(500);
+                    response.message = 'Oops! Some error occurred';
+                    response.code = 500;
+                    response.errors = err;
+                    console.log(err);
+                    res.json(JSON.stringify(response));
+                }else{
+                    if(rows.length){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }
+                    
+            });
+        }else{
+            return false;
+        }
     }
-}
+};
+
+    //private method
+    function genToken(user){
+        var token = jwt.sign(user, app.get('superSecret'),{
+            expiresInMinutes: app.get('loginExpiryMinutes') // expires in 24 hours
+        });
+        return {
+            token: token,
+            user: user,
+            expires: app.get('loginExpiryMinues')
+        }
+    }
+module.exports = auth;
