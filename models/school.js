cd1f6d5ed4df52580a4f1d6790cb260fad575346
ankits <ankuradhey@gmail.com>
2016-05-09 00:43:49 +0530

    remove query print

--- models/school.js
+++ models/school.js
@@ -18,7 +18,6 @@ var schools = {
         var query = 'SELECT s.school_id, s.school_name, board.board_name, s.school_medium, s.school_affiliation_code, s.school_address, s.school_img, s.school_desc as school_description FROM school s \n\
                      join board_master board on board.board_id = s.school_board and board.board_status = "1" \n\
                      where school_status = "1" ';
-        console.log(query);
         db.get().query(query, function (err, rows) {
             console.log('select query');
             if (err) 
