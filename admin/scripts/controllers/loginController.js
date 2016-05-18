86dc98b1b0bb8f387001836e93ce3c0a38c32031
ankuradhey <extramarks@extramarks-Ankit>
2016-05-18 16:49:13 +0530

    implemented login work from controller

---
+++ admin/scripts/controllers/loginController.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sbAdminApp.controller:loginController
+ * @author AnkitS
+ * @description
+ * # SchoolCtrl
+ * Controller of the sbAdminApp
+ */
+
+angular.module('sbAdminApp')
+        .controller("loginController", ["$scope", "$location", "$window", "authService", function ($scope, $location, $window, authService) {
+                $scope.userInfo = null;
+                $scope.login = function () {
+                    authService.login($scope.user.email, $scope.user.password)
+                            .then(function (result) {
+                                $scope.userInfo = result;
+                                $location.path("/");
+                            }, function (error) {
+                                $window.alert("Invalid credentials");
+                                console.log(error);
+                            });
+                };
+
+                $scope.cancel = function () {
+                    $scope.userName = "";
+                    $scope.password = "";
+                    $scope.role = "";
+                };
+            }]);
