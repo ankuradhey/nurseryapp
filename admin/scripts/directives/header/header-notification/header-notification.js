8fa86776a6f2bd03c208fb15cc0b535094df9256
ankuradhey <extramarks@extramarks-Ankit>
2016-05-18 16:49:39 +0530

    logout from admin popup link

--- admin/scripts/directives/header/header-notification/header-notification.js
+++ admin/scripts/directives/header/header-notification/header-notification.js
@@ -7,12 +7,18 @@
  * # adminPosHeader
  */
 angular.module('sbAdminApp')
-	.directive('headerNotification',function(){
-		return {
-        templateUrl:'scripts/directives/header/header-notification/header-notification.html',
-        restrict: 'E',
-        replace: true,
-    	}
-	});
+        .directive('headerNotification', ["authService", function (authService) {
+                return {
+                    templateUrl: 'scripts/directives/header/header-notification/header-notification.html',
+                    restrict: 'E',
+                    replace: true,
+                    link: function ($scope, element, attrs) {
+                        $scope.logout = function () {
+                            authService.logout()
+                                    .then(function (result) {}, function (error) {});
+                        }
+                    }
+                }
+            }]);
 
 
