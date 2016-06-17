b43fe62a9c71fd09b0591e1a02fbca1c93c4dff9
extramarks <extramarks@tech-ankits>
2016-06-17 13:17:43 +0530

    favorite query corrected

--- models/favorite.js
+++ models/favorite.js
@@ -23,7 +23,7 @@ exports.addFavorite = function(favParams, done){
 exports.getFavorite = function(userId, done){
     db.get().query('select fav_id, fav_parent_id, fav_school_id, s.school_name, s.school_affiliation_code, s.school_phone, s.school_address   from favorite_list f\n\
                     join school s on s.school_id = f.fav_school_id and s.school_status = "1" \n\
-                    where f.favorite_status = "1" and fav_parent_id = ? ',userId, function(err, rows) {
+                    where f.fav_status = "1" and fav_parent_id = ? ',userId, function(err, rows) {
             console.log(err);
             if (err)
                 return done(err)
