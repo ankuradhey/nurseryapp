51b3e90a41d041d7840270391a7f538029d4841e
ankits <ankuradhey@gmail.com>
2016-06-19 12:58:28 +0530

    parent passed in query

--- models/school.js
+++ models/school.js
@@ -29,9 +29,10 @@ var schools = {
     getAllActive: function(parentId, done) {
         var query = 'SELECT s.school_id, s.school_name, board.board_name, s.school_medium, s.school_affiliation_code, s.school_address, s.school_img, \n\
                     s.school_desc as school_description, if(fav.fav_id is not null,"1","0") as fav_status, avg(review_rating) as review_rating  FROM school s \n\
-                     join board_master board on board.board_id = s.school_board and board.board_status = "1" where school_status = "1" \n\
-                     join reviews r on r.review_school_id = s.school_id and r.review_status = "1" \n\
-                     left join favorite_list fav on s.school_id = fav.fav_school_id and fav.fav_parent_id = ? ';
+                     join board_master board on board.board_id = s.school_board and board.board_status = "1"  \n\
+                     left join reviews r on r.review_school_id = s.school_id and r.review_status = "1" \n\
+                     left join favorite_list fav on s.school_id = fav.fav_school_id and fav.fav_parent_id = ? \n\
+                     where school_status = "1" ';
         db.get().query(query,[parentId || 0], function(err, rows) {
             console.log('select query');
             if (err)
