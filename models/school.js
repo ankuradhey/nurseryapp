0efd76bde072891d901d35eee20c5f26816e9a69
ankits <ankuradhey@gmail.com>
2016-06-19 12:37:30 +0530

    rating passed in school list

--- models/school.js
+++ models/school.js
@@ -28,8 +28,9 @@ var schools = {
     },
     getAllActive: function(done) {
         var query = 'SELECT s.school_id, s.school_name, board.board_name, s.school_medium, s.school_affiliation_code, s.school_address, s.school_img, \n\
-                    s.school_desc as school_description, if(fav.fav_id is not null,"1","0") as fav_status  FROM school s \n\
+                    s.school_desc as school_description, if(fav.fav_id is not null,"1","0") as fav_status, avg(review_rating) as review_rating  FROM school s \n\
                      join board_master board on board.board_id = s.school_board and board.board_status = "1" where school_status = "1" \n\
+                     join reviews r on r.review_school_id = s.school_id and r.review_status = "1" \n\
                      left join favorite_list fav on s.school_id = fav.fav_school_id and fav.fav_parent_id = ? ';
         db.get().query(query, function(err, rows) {
             console.log('select query');
