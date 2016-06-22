222c83f9604fd690616e74a2a5117bd425b35a2d
ankits <ankuradhey@gmail.com>
2016-06-22 21:25:02 +0530

    corrected favorite bug

--- models/favorite.js
+++ models/favorite.js
@@ -28,14 +28,20 @@ exports.removeFavorite = function(favParams, done){
 }
 
 exports.getFavorite = function(userId, schoolId,done){
-    console.log('select fav_id, fav_parent_id, fav_school_id, s.school_name, s.school_affiliation_code, s.school_phone, s.school_address from favorite_list f\n\
-                    join school s on s.school_id = f.fav_school_id and s.school_status = "1" \n\
-                    where f.fav_status = "1" and fav_parent_id = ? and fav_school_id = ? ');
-    
-    
     db.get().query('select fav_id, fav_parent_id, fav_school_id, s.school_name, s.school_affiliation_code, s.school_phone, s.school_address from favorite_list f\n\
                     join school s on s.school_id = f.fav_school_id and s.school_status = "1" \n\
                     where f.fav_status = "1" and fav_parent_id = ? and fav_school_id = ? ',[userId, schoolId], function(err, rows) {
+            console.log(err);
+            if (err)
+                return done(err)
+            return done(null, rows);
+        })
+}
+
+exports.getFavoriteByUser = function(userId, done){
+    db.get().query('select fav_id, fav_parent_id, fav_school_id, s.school_name, s.school_affiliation_code, s.school_phone, s.school_address from favorite_list f\n\
+                    join school s on s.school_id = f.fav_school_id and s.school_status = "1" \n\
+                    where f.fav_status = "1" and fav_parent_id = ?  ',[userId], function(err, rows) {
             console.log(err);
             if (err)
                 return done(err)
