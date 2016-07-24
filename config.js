ffa5bf39c97c9e1b0264314605f4aae2e3fecf5f
ankits <ankuradhey@gmail.com>
2016-07-25 02:14:52 +0530

    base url changed for page request

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     'mode': 'development', // production | development
     'loginExpiryMinutes':1440, //24*60
     'loginExpirySeconds': 604800 || 86400, //60*60*24
-    'baseUrl':'http://ec2-54-201-182-195.us-west-2.compute.amazonaws.com/nurseryapp',
+    'baseUrl':'http://ec2-54-201-182-195.us-west-2.compute.amazonaws.com:7071',
     mailSettings:{
         username:"ankuradhey@gmail.com",
         password:"alldayilovesports"
