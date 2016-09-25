3cddaa64de14ce422fcae88696d748c5e6e2b3ea
Ankit <ankuradhey@gmail.com>
2016-09-25 12:05:30 +0530

    school image bug fix

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     'loginExpiryMinutes':1440, //24*60
     'loginExpirySeconds': 604800 || 86400, //60*60*24
     'baseUrl':'http://ec2-54-201-182-195.us-west-2.compute.amazonaws.com:7071',
-    'imageUrl':'http://ec2-54-201-182-195.us-west-2.compute.amazonaws.com:7071/nurseryapp/uploads',
+    'imageUrl':'http://ec2-54-201-182-195.us-west-2.compute.amazonaws.com/nurseryapp/uploads',
     mailSettings:{
         username:"ankuradhey@gmail.com",
         password:"alldayilovesports"
